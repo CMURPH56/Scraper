2646dd54692d19a6edec23b1a10a2241dc38639f
Cam Murphy <CamMurphy195@gmail.com>
2019-05-13 21:27:56 -0500

    was able to pull sub titles, working on actual titles

--- webscraper.py
+++ webscraper.py
@@ -10,13 +10,9 @@ content = BeautifulSoup(response.content, "html.parser")
 tweetArr = []
 for tweet in content.findAll('div', attrs={"class": "MomentCapsuleSummary-details"}):
   print(tweet)
-  '''
-    tweetObject = {
-        "title": tweet.find('span', attrs={"class": "MomentCapsuleSummary-details"}).text,
-        "link": tweet.find('h5', attrs={"class": "dateTime"}).text,
-        "description": tweet.find('p', attrs={"class": "content"}).text,
-    }
-    tweetArr.append(tweetObject)
+  tweetObject = {
+      "title": tweet.find('span', attrs={"class": "MomentCapsuleSubtitle-context"}).text,
+  }
+  tweetArr.append(tweetObject)
 with open('twitterData.json', 'w') as outfile:
     json.dump(tweetArr, outfile)
-    '''
